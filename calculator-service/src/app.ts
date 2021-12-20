feb3c6971a702a6c97f0ba302693ca2274d6acdf
Ricky Chon <rickychon99@gmail.com>
2021-12-20 03:35:52 -0500

    Add temp route to API root

--- calculator-service/src/app.ts
+++ calculator-service/src/app.ts
@@ -6,13 +6,16 @@ import { expressCallback } from './frameworks';
 
 require('dotenv').config();
 
-const apiRoot = process.env.CALCULATOR_API_ROOT || '/calculator/api/v1';
+const apiRoot = process.env.CALCULATOR_API_ROOT || '/calc/api/v1';
 const port = process.env.CALCULATOR_API_PORT || 8080;
 const app = express();
 
 app.use(bodyParser.json()); // application/json
 app.use(bodyParser.urlencoded({ extended: true })); // application/x-www-form-urlencoded
 
+app.get(apiRoot, (req, res) => {
+  res.send('API root for calculator service');
+});
 app.get(`${apiRoot}/sub`, expressCallback(getDifference));
 
 app.listen(port, () => {
